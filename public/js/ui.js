ca9338fceae2dcb932ef706b3dd1573d57ea5cf0
Evan Owen <kainosnoema@gmail.com>
2013-06-14 01:49:14 -0700

    Use Dropmail.Model.form for binding to submit event
    Signed-off-by: Evan Owen <kainosnoema@gmail.com>

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,46 +1,23 @@
+window.ui = {};
 
-function onSubmit($form, model, callback){
-  $form.on('submit', function(){
-    model.create(serializeObject($form), respond($form, callback));
-    return false;
-  });
-}
-
-function serializeObject($form){
-  var params = {};
-
-  $.each($form.serializeArray(), function(){
-    params[this.name] = this.value;
-  });
-
-  return params;
-}
-
-function respond($form, callback){
+ui.handleValidation = function(err, $form){
   var $button = $(this).children('button')
     , buttonText = $button.text();
 
-  return function(err, object){
-    $form.find('.control-group').removeClass('error');
-    $form.find('.help-block').remove();
+  $form.find('.control-group').removeClass('error');
+  $form.find('.help-block').remove();
 
-    if (err) {
-      var errors = err.attributes;
+  var errors = err.attributes;
 
-      for (var name in errors) {
-        var input = $('input[name=' + name + ']');
-        input.closest('.control-group').addClass('error');
-        input.after("<div class='help-block'>" + toSentence(errors[name]) + '</div>');
-      }
+  for (var name in errors) {
+    var input = $('input[name=' + name + ']');
+    input.closest('.control-group').addClass('error');
+    input.after("<div class='help-block'>" + toSentence(errors[name]) + '</div>');
+  }
 
-      $button.attr('disabled', false).text(buttonText);
-      return;
-    }
+  $button.attr('disabled', false).text(buttonText);
 
-    if (callback) callback(err, object);
-  };
-}
-
-function toSentence(array){
-  return array.join(', ');
-}
+  function toSentence(array){
+    return array.join(', ');
+  }
+};
