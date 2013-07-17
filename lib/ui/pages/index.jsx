9d7968c9c0f82584061dd0f784633fc9c20ed30e
Stephen Celis <stephen@stephencelis.com>
2013-07-16 19:03:39 -0700

    Extract orgs page from index
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- lib/ui/pages/index.jsx
+++ lib/ui/pages/index.jsx
@@ -8,60 +8,7 @@ Pages.Index = React.createClass({
   render: function(){
     if (!this.props.user) return Pages.MarketingSplash(this.props);
 
-    return (
-      <div class='row'>
-        <div class='span8'>
-          {this.listOrganizations()}
-        </div>
-        <div class='span4'>
-          <h3>Create an organization</h3>
-          <form method='post' ref='form' class='signup' onSubmit={this.handleSubmit}>
-            <FormField
-              label='Name'
-              name='name'
-              placeholder='Name'
-              errors={this.state.errors.name} />
-            <FormSubmit label='Create Organization' />
-          </form>
-        </div>
-      </div>
-    );
-  },
-
-  // private
-
-  handleSubmit: function(){
-    var self = this
-      , $form = $(this.refs.form.getDOMNode());
-
-    app.client.Organization.save($form, function(err, org){
-      if (err) return self.setState({ errors: err.attributes });
-      self.props.onOrgChange(org);
-    });
-
-    return false;
-  },
-
-  listOrganizations: function(){
-    var orgs = this.props.organizations;
-
-    if (!orgs.length) {
-      return (
-        <div>
-          <h3>Welcome to Dropmail</h3>
-          <div>To get started, create an organization.</div>
-        </div>
-      );
-    }
-
-    return (
-      <div>
-        <h3>Current Organizations</h3>
-        <OrganizationList
-          organizations={orgs}
-          onOrgChange={this.props.onOrgChange} />
-      </div>
-    );
+    return Pages.Organizations(this.props);
   }
 
 });
