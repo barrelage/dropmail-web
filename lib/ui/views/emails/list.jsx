41ffdfbc2b871205736f299b8b4155f0510d8ca1
Tyler Smalley <tylersmalley@me.com>
2013-12-10 19:09:48 -0800

    Checkpoint
    Signed-off-by: Tyler Smalley <tylersmalley@me.com>

--- lib/ui/views/emails/list.jsx
+++ lib/ui/views/emails/list.jsx
@@ -2,7 +2,77 @@
 if (Views.Emails == undefined) Views.Emails = {};
 
 Views.Emails.List = React.createClass({
+
+  getInitialState: function() {
+    this.fetchEmails();
+    return { emails: null };
+  },
+
   render: function() {
-    return <h2>List Emails</h2>;
-  }
+    var self = this
+      , emails = this.state.emails;
+
+    function listItem(email) {
+      return (
+        <tr onClick={self.showEmail.bind(this, email)}>
+          <td>{email.get('to')}</td>
+          <td>{email.get('subject')}</td>
+          <td></td>
+        </tr>
+      );
+    }
+
+    if (!emails) {
+      return (
+        <div class='alert alert-info'>
+          Loading...
+        </div>
+      )
+    }
+
+    if (!emails[0]){
+      return (
+        <div class='alert'>
+          No emails have been sent.
+          You can send a sample email from the template editor.
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <h2>Emails</h2>
+          <table class='table table-striped'>
+            <thead>
+              <tr>
+                <th>To</th>
+                <th>Subject</th>
+                <th>Created At</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {emails.map(listItem)}
+            </tbody>
+          </table>
+      </div>
+    );
+  },
+
+  // private
+
+  fetchEmails: function() {
+    var self = this;
+
+    app.client.Email.fetch(function(err, emails) {
+      if (err) return console.error(err);
+      self.setState({ emails: emails });
+    });
+  },
+
+  showEmail: function(email) {
+    app.goTo('emails/' + email.get('id'));
+    return false;
+  },
+
 });
