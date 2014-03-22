27793317b0d2f2de33057f5e6158376e9311e146
Tyler Smalley <tylersmalley@me.com>
2014-03-21 17:34:16 -0700

    Break out email list to table element
    Signed-off-by: Tyler Smalley <tylersmalley@me.com>

--- lib/ui/views/emails/list.jsx
+++ lib/ui/views/emails/list.jsx
@@ -9,52 +9,12 @@ Views.Emails.List = React.createClass({
   },
 
   render: function() {
-    var self = this
-      , emails = this.state.emails;
-
-    function listItem(email) {
-      return (
-        <tr onClick={self.showEmail.bind(this, email)}>
-          <td>{email.get('to')}</td>
-          <td>{email.get('subject')}</td>
-          <td>{email.get('send_at')}</td>
-        </tr>
-      );
-    }
-
-    if (!emails) {
-      return (
-        <div class='alert alert-info'>
-          Loading...
-        </div>
-      )
-    }
-
-    if (!emails[0]){
-      return (
-        <div class='alert'>
-          No emails have been sent.
-          You can send a sample email from the template editor.
-        </div>
-      );
-    }
+    var self = this;
 
     return (
       <div>
         <h2>Emails</h2>
-          <table class='table table-striped'>
-            <thead>
-              <tr>
-                <th>To</th>
-                <th>Subject</th>
-                <th>Sent At</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {emails.map(listItem)}
-            </tbody>
-          </table>
+        <EmailTable emails={this.state.emails} />
       </div>
     );
   },
